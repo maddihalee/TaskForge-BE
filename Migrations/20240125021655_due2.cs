9930678f40c8488ca6df29fd9dcc76e222016de9
Maddi Cook <maddihcook@gmail.com>
2024-01-24 20:19:39 -0600

    started the date picker

---
+++ Migrations/20240125021655_due2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskForge.Migrations
+{
+    /// <inheritdoc />
+    public partial class due2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Due",
+                table: "Tasks",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Due",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Due",
+                table: "Tasks");
+        }
+    }
+}
